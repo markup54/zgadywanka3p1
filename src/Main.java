c2f6cc49a089797e7f790dd4a492de8a16915016
markup54 <marzenakupka@tlen.pl>
2024-09-04 15:45:20 +0200

    dodatkowy komnikat

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     //Nazwa pliku i nazwa klasy muszą być takie same
     //nazwy klas powinny być z wielkiej litery - tak karze obyczaj
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Witamy na lekcjach programowania");
     }
 }
