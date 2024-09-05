6747fece5ad848ae554004aa8588ee98eae1656e
markup54 <marzenakupka@tlen.pl>
2024-09-05 12:29:57 +0200

    wczytanie liczby z klawiatury

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     //Nazwa pliku i nazwa klasy muszą być takie same
     //nazwy klas powinny być z wielkiej litery - tak karze obyczaj
@@ -6,9 +8,20 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Witamy na lekcjach programowania");
         //typ prosty - tylko wartośc, z małej litery
+        /*
+        boolean,
+        int long  short byte
+        float double
+        char
+         */
         //typ zlozony - obiekt ktory ma walansosci i metody, z wielkiej litery
         int wylosowanaLiczba = (int)(Math.random()*10+1);//rzutowanie
 
         System.out.println(wylosowanaLiczba);
+
+        System.out.println("Zgadnij liczbę");
+        Scanner klawiatura = new Scanner(System.in);
+        int wpisanaLiczba = klawiatura.nextInt();
+
     }
 }
